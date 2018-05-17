4feafbb547ea5ec2ee5f9f24da5734b67b8b8358
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-05-16 23:27:49 -0700

    Fix sidebar + add calendar

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var navStatus = false; //This is the state of the navbar (horizontal/vertical)
 
 $(function(){
+    if ($(window).scrollTop() != 0 && !navStatus){
+      navStatus = true;
+        $("nav").css({
+          "flexDirection":"column",
+          "top":"50%",
+          "transform":"translateY(-50%)",
+          "left":"auto",
+          "right":"5vw",
+          "bottom":"initial",
+          "height":"40vmin",
+          "width":"18vmin",
+          "margin":0
+        });
+        $("nav a").css({"fontSize":"4vmin"});
+    }
   $(window).scroll(function(){
     if ($(this).scrollTop() != 0 && !navStatus){
       navStatus = true;
@@ -13,7 +28,8 @@ $(function(){
           "right":"-18vmin",
           "bottom":"initial",
           "height":"40vmin",
-          "width":"18vmin"
+          "width":"18vmin",
+          "margin":"0"
         }); //Set CSS before reappearing
         $("nav a").css({"fontSize":"4vmin"});
         $("nav").show(100).animate({
@@ -29,11 +45,11 @@ $(function(){
           "top":"auto",
           "left":"50%",
           "right":"auto",
-          "bottom":"-14vmin",
-          "height":"14vmin",
+          "bottom":"-20vmin",
+          "height":"20vmin",
           "width":"80vw"
         }); //Set CSS before reappearing
-        $("nav a").css({"fontSize":"4vmin"});
+        $("nav a").css({"fontSize":"6vmin"});
         $("nav").show(100).animate({
           "bottom":"0"
         },200);
